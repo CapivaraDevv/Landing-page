27cab9e4a6d602fe0df7509830babe1217ac40b9
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2025-10-14 01:54:05 -0300

    Update Vite dependency to use caret versioning and enhance App component structure

--- Brewly/src/App.jsx
+++ Brewly/src/App.jsx
@@ -1,15 +1,18 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import Hero from './components/Hero'
 import Products from './components/Products'
+import About from './components/About';
 
 function App() {
-  
+  const [search, setSearch] = useState('');
 
   return (
     <>
-      <Header/>
+      <Header search={search} setSearch={setSearch} />
       <Hero/>
-      <Products/>
+      <Products search={search} />
+      <About/>
     </>
   )
 }
