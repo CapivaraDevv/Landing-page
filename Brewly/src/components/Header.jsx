a4393b1138b488b9e7e4a7468382438a1e7e6de4
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2025-10-21 16:09:11 -0300

    UI/UX Changes on header

--- Brewly/src/components/Header.jsx
+++ Brewly/src/components/Header.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-
+import "../styles/Header.css"
 
 
 
@@ -14,15 +14,20 @@ function Header({ search, setSearch }) {
         <>
             <div className="fixed top-0 left-0 right-0 w-full bg-[#856a59] backdrop-blur border-b border-zinc-200 z-50">
                 <div className="max-w-6xl py-3 flex items-center gap-8">
-                    <a href="#" className="font-pacifico mx-7 text-3xl text-white underline-animated">Brewly</a>
+                    <a href="#" className="logo font-pacifico mx-7 text-3xl text-white underline-animated">Brewly</a>
                     <a href="#sobre" className="playfair-display-texto text-lg text-white underline-animated">Sobre nós</a>
                     <div className="relative group">
-                        <a href="#" className="playfair-display-texto text-lg text-white transition-colors duration-300 cursor-pointer">
+                        <a href="#" className="playfair-display-texto text-lg text-white cursor-pointer">
                             Produtos
                         </a>
 
-                        <div className="absolute top-full left-1/2 transform -translate-x-1/2 mt-3 w-30 bg-[#856a59] rounded-b-lg shadow-lg opacity-0 invisible group-hover:opacity-100 group-hover:visible transition-all duration-300 ease-in-out z-50">
-                            <div className="playfair-display-texto text-white text-center py-2">
+                        <div className="
+                            absolute top-full left-1/2 transform -translate-x-1/2 translate-y-2 ease-out
+                            scale-95 mt-3 w-30 bg-[#856a59] rounded-b-lg shadow-xl opacity-0 delay-75        
+                            invisible group-hover:opacity-100 group-hover:scale-100 group-hover:translate-y-0 
+                            group-hover:visible backdrop-blur-md transition-all duration-300 hover:ease-in
+                            ">
+                            <div className="products-backdrop playfair-display-texto text-white text-center py-2">
                                 <a href="#expresso" className="block px-4 py-2 text-sm  hover:bg-amber-50 hover:text-[#5A3E36] transition-colors duration-200">
                                     Expresso
                                 </a>
