cefca0e777fafb164d878fbe31624f374f3199fa
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2026-01-08 13:41:03 -0300

    Add animations in view off hero

--- Brewly/src/components/Hero.jsx
+++ Brewly/src/components/Hero.jsx
@@ -1,13 +1,29 @@
 import cafeImage from '../assets/xicara-de-cafe-em-um-prato.png'
-import imagemFundo from '../assets/Fundo.jpg'
+import { motion } from 'framer-motion'
 import '../styles/Hero.css'
 
+const cardVariants = {
+    offScreen: { opacity: 0, y: 20 },
+    onScreen: {
+        opacity: 1,
+        y: 0,
+        transition: { bounce: 0.4, duration: 0.8 },
+    },
+}
+
 function Hero() {
+    
+
     return (
         <>
-            <section className="w-full h-150 pt-10 bg-[#5e4b3e] content-center">
+            <section className="w-full h-230 bg-[#F5EFE6] content-center">
                 <div className="mx-auto max-w-6xl px-4 py-30 flex items-center">
-                    <div className="text-white text-center md:text-left max-w-xl mx-auto">
+                    <motion.div
+                        variants={cardVariants}
+                        initial="offScreen"
+                        whileInView="onScreen"
+                        className="text-[#2B2B2B] text-center md:text-left max-w-xl mx-auto"
+                    >
                         <h2 className="playfair-display-texto text-4xl md:text-5xl font-semibold mb-4">
                             Conheça a <span className="font-pacifico">Brewly</span>
                         </h2>
@@ -15,31 +31,9 @@ function Hero() {
                             A <span className="font-pacifico">Brewly</span> é uma empresa que traz conforto e uma explosão de sabores
                             em seus produtos, tornando cada um de nossos produtos uma experiência fantástica.
                         </p>
-                    </div>
-                    <div className="relative w-52 h-52 flex justify-center items-end">
-                        <img src={cafeImage} alt="Xícara de café Brewly" className="w-52" />
-
-                        {/* Container de fumaça */}
-                        <div className="absolute bottom-42 left-1/2 -translate-x-1/2 w-full h-full">
-                            <span className="fumaça offset-left delay-0"></span>
-                            <span className="fumaça offset-center delay-200"></span>
-                            <span className="fumaça offset-right delay-400"></span>
-                            <span className="fumaça offset-left delay-600"></span>
-                            <span className="fumaça offset-center delay-800"></span>
-                            <span className="fumaça offset-right delay-1000"></span>
-                        </div>
-                    </div>
-                </div>
-                <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 text-center'>
-                    <div className='mx-auto'>
-                        <h1 className='bg-amber-100 rounded-full mx-7'>+ 50</h1>
-                        <h3 className=''>Produtos vendidos</h3>
-                    </div>
-                    <div className=''>
-                        <img src="" alt="" className='bg-amber-100 rounded-full p-4'/>
-                    </div>
-                    <div className=''>
-                        <img src="" alt="" className='bg-amber-100 rounded-full p-4'/>
+                    </motion.div>
+                    <div className="imagem">
+                        <img src={cafeImage} alt="Xícara de café" />
                     </div>
                 </div>
             </section>
