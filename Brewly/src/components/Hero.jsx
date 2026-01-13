6c8ab03af091a84f7a85048493919ebc24b77dcc
PedroHenrique <pedro.henrique.pereira.2014@gmail.com>
2026-01-13 17:33:29 -0300

    Better composition on react components

--- Brewly/src/components/Hero.jsx
+++ Brewly/src/components/Hero.jsx
@@ -1,6 +1,6 @@
-import cafeImage from '../assets/Cafe.jpg'
 import { motion } from 'framer-motion'
 import '../styles/Hero.css'
+import ProductsCard from './ProductsCard'
 
 const cardVariants = {
     offScreen: { opacity: 0, y: 20 },
@@ -16,8 +16,8 @@ function Hero() {
 
     return (
         <>
-            <section className="w-full h-250 bg-[#F5EFE6] content-center">
-                <div className="mx-auto max-w-6xl px-4 flex items-center mb-15">
+            <section className="w-full h-auto bg-[#F5EFE6] content-center ">
+                <div className="mx-auto max-w-6xl px-4 flex items-center mb-15 mt-52">
                     <motion.div
                         variants={cardVariants}
                         initial="offScreen"
@@ -33,35 +33,7 @@ function Hero() {
                         </p>
                     </motion.div>
                 </div>
-                <div className="grid grid-cols-2 gap-8 items-center max-w-6xl mx-auto px-4">
-                    {/* Coluna 1: Imagem com Texto sobreposto */}
-                    <div className="relative w-full h-[300px]">
-                        <div className="imagem overflow-hidden rounded-lg w-full h-full relative">
-                            <img
-                                src={cafeImage}
-                                alt="Xícara de café"
-                                className="block w-full h-full object-cover"
-                            />
-                            <div className='absolute inset-0 rounded-lg w-full z-10 bg-black/55'></div>
-                            
-                            {/* Texto sobreposto à imagem */}
-                            <h1 className='
-                                absolute inset-0 z-20 flex items-center justify-center
-                                text-[80px] font-black uppercase bg-clip-text 
-                                text-transparent text-image hover:text-white 
-                                transition-all duration-400 ease-in-out
-                                '
-                                style={{ backgroundImage: `url(${cafeImage})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
-                                Coffee
-                            </h1>
-                        </div>
-                    </div>
-
-                    {/* Coluna 2: Conteúdo adicional (opcional) */}
-                    <div className="flex flex-col gap-4">
-                        {/* Adicione mais conteúdo aqui se necessário */}
-                    </div>
-                </div>
+                <ProductsCard />
             </section>
         </>
     )
